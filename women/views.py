d30c4565a4f228a7eec72f81a2ffacbb732c47e6
xartiou <xartiou@gmail.com>
2022-01-17 17:26:07 +0300

    Templates_Начало https://djbook.ru/rel3.0/topics/templates.html

--- women/views.py
+++ women/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .models import *
 
 # Create your views here.
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts,'menu':menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'women/about.html', {'menu':menu, 'title': 'О сайте'})
 
 def categories(request, cat_id):
     if(request.GET):
