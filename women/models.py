f0f05f7b7a726f70929d773da5df8a2925275e9c
xartiou <xartiou@gmail.com>
2022-01-17 22:57:34 +0300

    Начало работы с admin

--- women/models.py
+++ women/models.py
@@ -9,25 +9,36 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="время редактирования")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категории")
 
-# метод вывода информации объекта
+    # вложенный класс для настройки модели
+    class Meta:
+        verbose_name = 'Известные женщины'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['time_create', 'title']
+
+    # метод вывода информации объекта
     def __str__(self):
         return self.title
 
-# метод для формирования url
+    # метод для формирования url
     def get_absolute_url(self):
-        return reverse ('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_id': self.pk})
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="категория")
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['id']
 
     def __str__(self):
         return self.name
@@ -35,7 +46,6 @@ class Category(models.Model):
     # метод для формирования url
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
-
 
 # from women.models import Women команда для shell
 # from django.db import connection
