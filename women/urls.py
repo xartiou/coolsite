9be0d4448c4010a642c3d139f8bcf24805ec6b0e
xartiou <xartiou@gmail.com>
2022-01-17 17:26:07 +0300

    Templates_Начало https://djbook.ru/rel3.0/topics/templates.html

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, re_path
-from .views import index, categories, archive
+from .views import index, categories, archive, about
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<int:cat_id>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
 ]
