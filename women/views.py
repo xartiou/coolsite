5af9ac9545b65680132841b665fed00d3391098d
xartiou <xartiou@gmail.com>
2022-01-18 17:41:09 +0300

    Пользовательские теги шаблонов

--- women/views.py
+++ women/views.py
@@ -12,10 +12,8 @@ menu = [
 ]
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'cat_selected': 0,
         'title': 'Главная страница'
@@ -43,14 +41,11 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
-
     if len(posts) == 0:
         raise Http404()
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'cat_selected': cat_id,
         'title': 'Отображение по рубрикам'
