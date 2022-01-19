b0d92a09392ba977b41cd24cfb1ce9bad46ebdf6
xartiou <xartiou@gmail.com>
2022-01-19 16:55:59 +0300

    Использование форм не связанных с моделями

--- women/views.py
+++ women/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -24,7 +25,18 @@ def about(request):
     return render(request, 'women/about.html', {'menu':menu, 'title': 'О сайте'})
 
 def addpage(request):
-    return HttpResponse("Добавить статью")
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None,'Ошибка добавления статьи')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form':form, 'menu':menu, 'title': 'Добавление статьи'})
 
 def contact(request):
     return HttpResponse("Обратная связь")
